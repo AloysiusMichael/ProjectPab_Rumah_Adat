914c6c183494fd2ccca1ea5057f3bd3297e1e219
MichaelSaputra1 <michaels@mhs.mdp.ac.id>
2023-12-29 22:38:58 +0700

    Tambahan rumah
    penambahan rumah adat

--- prj_rumah_adat/lib/models/favorit.dart
+++ prj_rumah_adat/lib/models/favorit.dart
@@ -40,6 +40,27 @@ class Favorit extends ChangeNotifier {
             "Rumah adat Mbaru Niang memiliki ciri yaitu pada bagian atap, bentuknya menyerupai kerucut menjulang tinggi dari atas hingga permukaan tanah. Kerucut memiliki arti yaitu perlindungan dan persatuan masyarakat desa Wae Rebo.    ",
         imagePath: 'images/rmh_mbaruniang.jpg',
         dibangun: '1800-an'),
+    Rumah(
+        nama: "Rumah Nuwo Sesat",
+        asal: "Lampung",
+        deskripsi:
+            "Rumah adat Nuwo Sesat berfungsi sebagai tempat pertemuan adat bagi para purwatin (Penyimbang) pada saat mengadakan pepung adat (Musyawarah).",
+        imagePath: 'images/rmh_nuwosesat.jpeg',
+        dibangun: '1800-an'),
+    Rumah(
+        nama: "Rumah Baduy",
+        asal: "Banten",
+        deskripsi:
+            "Rumah adat Baduy terdiri atas tiga bagian utama. Bagian pertama disebut Sosoro atau bagian depan untuk menerima tamu dan tempat menenun oleh perempuan, bagian kedua merupakan ruang inti keluarga yang bisa dipakai bercengkerama sampai tertidur, dan bagian rumah yang ketiga adalah Ipah untuk menyimpan hasil sawah serta alat bertani.",
+        imagePath: 'images/rmh_baduy.jpeg',
+        dibangun: '1500-an'),
+    Rumah(
+        nama: "Rumah Kebaya",
+        asal: "DKI Jakarta",
+        deskripsi:
+            "Rumah adat Kebaya dipengaruhi oleh akulturasi dari berbagai daerah baik lokal maupun internasional. Ciri rumah ini yakni teras yang luas sebagai tempat untuk menjamu tamu dan bersantai keluarga",
+        imagePath: 'images/rmh_kebaya.jpeg',
+        dibangun: '1930'),
   ];
 
   // state awal
